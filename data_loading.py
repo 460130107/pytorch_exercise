5391a2128c9f90d0bd63f10d39f308001fc14aab
styang <460130107@qq.com>
2019-03-12 07:27:46 +0800

    add more exercise code ....

--- data_loading.py
+++ data_loading.py
@@ -54,3 +54,52 @@ if __name__ == "__main__":
     plt.figure()
     show_landmarks(io.imread(os.path.join('faces/faces/', img_name)), landmarks)
     plt.show()
+
+    class FaceLandmarksDataset(Dataset):
+        """Face Landmarks dataset."""
+
+        def __init__(self, csv_file, root_dir, transform=None):
+            """
+            Args:
+                csv_file (string): Path to the csv file with annotations.
+                root_dir (string): Directory with all the images.
+                transform (callable, optional): Optional transform to be applied
+                    on a sample.
+            """
+            self.landmarks_frame = pd.read_csv(csv_file)
+            self.root_dir = root_dir
+            self.transform = transform
+
+        def __len__(self):
+            return len(self.landmarks_frame)
+
+        def __getitem__(self, idx):
+            image_name = os.path.join(self.root_dir, self.landmarks_frame.iloc[idx, 0])
+            image = io.imread(image_name)
+            landmarks = self.landmarks_frame.iloc[idx, 1:].as_matrix()
+            landmarks = landmarks.astype("float").reshape(-1,2)
+            sample = {"image":image, "landmarks":landmarks}
+            if self.transform:
+                sample = self.transform(sample)
+            return sample
+
+
+    # Let’s instantiate this class and iterate through the data samples.
+    # We will print the sizes of first 4 samples and show their landmarks
+    face_dataset = FaceLandmarksDataset(csv_file='faces/faces/face_landmarks.csv',
+                                        root_dir='faces/faces/')
+    fig = plt.figure()
+    for i in range(len(face_dataset)):
+        sample = face_dataset[i]
+
+        print(i, sample['image'].shape, sample['landmarks'].shape)
+
+        ax = plt.subplot(1, 4, i + 1)
+        plt.tight_layout()
+        ax.set_title('Sample #{}'.format(i))
+        ax.axis('off')
+        show_landmarks(**sample)
+
+        if i == 3:
+            plt.show()
+            break
